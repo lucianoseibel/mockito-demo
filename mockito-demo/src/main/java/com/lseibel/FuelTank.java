af3836fc9156e7a98eb7c7b257c39eafe8573b22
Luciano Seibel <luciano.seibel@gmail.com>
2022-09-15 18:05:28 -0300

    first

---
+++ mockito-demo/src/main/java/com/lseibel/FuelTank.java
@@ -0,0 +1,12 @@
+package com.lseibel;
+
+public class FuelTank {
+    private int fuel;
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+}
